515c368d9c63061e93520e0c80d3f50a8cc16298
Jhon Sebastian Navas Chafuelan <jsnavas@cenicana.org>
2023-02-07 12:02:03 -0500

    V1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <button @click="show = !show">Menú</button>
+  <transition name="fade">
+    <Menu v-show="show" />
+  </transition>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <div>
+    <Modal/>
+  </div>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <div>{{ text }}</div>
+  <div>{{ algo }}</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import { defineAsyncComponent } from "vue";
+// const HelloWorld = defineAsyncComponent(() =>
+//   import(`./components/HelloWorld.vue`)
+// );
+import Menu from "./components/Menu.vue";
+import Modal from "./components/Modal.vue";
+import base from "@/mixins/base";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Menu,
+    Modal
   },
+  mixins: [base],
+  data() {
+    return {
+      text: "Hola Vue",
+      show: false,
+    };
+  },
+  beforeCreate(){
+    console.log("beforeCreate", this.$data, this.$el);
+  }, created() {
+    console.log("created", this.$data, this.$el);
+  },
+  mounted() {
+    console.log("mounted", this.$data, this.$el);
+  },
+  // data() {
+  //   return {
+  //     componente: "HelloWorld",
+  //   };
+  // },
 };
 </script>
 
@@ -22,5 +66,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-active,
+.fade-enter-active {
+  transition: opacity 0.5s ease;
 }
 </style>
